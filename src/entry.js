7eb31b0c2d60eeaafb99a06075ede142e32942bd
Miguel Reyes <asiessoyeltier1@gmail.com>
2023-10-16 18:57:52 -0500

    [boilerplate] Static assets and library code

--- src/entry.js
+++ src/entry.js
@@ -1,14 +1,15 @@
-// Adds all the dependencies the final
-
 const SCRIPT = "script";
 
+// predefines paths
 paths = [ "src/p5.min.js", "src/sketch.js"];
 
 for(let i = 0; i < paths.length; ++i)
 {
-		let script = document.createElement(SCRIPT);
+  // html element 
+  let script = document.createElement(SCRIPT);
 
-		script.src = paths[i];
+  script.src = paths[i];
 
-		document.body.appendChild(script);
+  //apend it to load
+  document.body.appendChild(script);
 }
