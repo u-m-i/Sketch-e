9a368458e01269ae90bd69dfd177741289d44b54
Miguel Reyes <asiessoyeltier1@gmail.com>
2023-10-17 23:55:01 -0500

    [boilerplate] Robot added to the project

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,52 @@
 
-let i = 0;
+// Starts at the begining
+function setup()
+{
+	createCanvas(500,500);
+}
 
+// Excuted each frame
 function draw()
 {
-		i++;
-		console.log(i);
+	strokeWeight(6);
+
+	//robots head
+	fill(200);
+	rect(100, 100, 300, 300, 20);
+
+
+	//robots antenna
+	fill(250, 250, 0);
+	ellipse(250, 70, 60, 60);
+
+	fill(200, 0, 200);
+	rect(210, 80, 80, 30);
+
+	//robots eyes
+	fill(255);
+	ellipse(175, 200, 80, 80);
+	point(175, 200);
+	ellipse(325, 200, 80, 80);
+	point(325, 200);
+
+
+	//robots nose
+	fill(255, 0, 0);
+	triangle(250, 220, 200, 300, 300, 300);
+
+	//robots ears
+	rect(80, 180, 30, 100);
+	rect(390, 180, 30, 100);
+
+	//robots mouth
+	noFill();
+	beginShape();
+	vertex(175, 340);
+	vertex(200, 370);
+	vertex(225, 340);
+	vertex(250, 370);
+	vertex(275, 340);
+	vertex(300, 370);
+	vertex(325, 340);
+	endShape();
 }
